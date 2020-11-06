d300340209b90df005bd63230c0bc0704077d33e
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2020-11-06 20:46:09 +0100

    Rozwiązanie uzupelnianki.

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -6,6 +6,6 @@ public class Exceptions {
         //Wyjątki rzucamy korzystając z instrukcji TRY CATCH, jeśli rzucamy wyjątek kontrolowany musimy dodatkowo w sygnaturze
         // metody/konstruktora zadeklarować wyjątek używając słowa THROWS
         //Do bloku try-catch możemy dodać dodatkowo blokFINALLY, który wykona się zawsze, niezależnie, czy w bloku try wyjątek
-        // wystąpił, czy nie
+        // wystąpił, czy nie.
     }
 }
